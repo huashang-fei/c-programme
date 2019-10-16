dcdc859584c17dacb8c09e59a0b1ccc490db4b78
= <15000234967@163.com>
2019-10-16 19:42:52 +0800

    U

---
+++ cday02.cpp
@@ -0,0 +1,27 @@
+// std: standard library io : int put and out put
+#include <stdio.h>
+// int :The return value of the function is an integer
+// main: there is only one main function 
+// void:function has no parameters
+//printf: print string function 
+// /n: wrap
+// ; :the statments end 
+// return:in the main function it stand for the end of the programme 
+// 0 : the return value of the fuction is zero 
+int main(void)
+{
+	printf("hello world \n") ;
+	printf("hello world ") ;
+	return 0 ;
+}
+/* #define p 3  the second way to define constant 
+int main()
+{
+   int a = 10 ;                 Define a variable and assign it
+   const int b = 11 ;           define a constant and it cannot be changed 
+}
+*/
+
+
+
+
